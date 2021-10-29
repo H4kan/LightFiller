4c847ab53838312af9aea86572a13cc6d7b12926
H4kan <szymon.sieradzki184@wp.pl>
2021-10-29 02:07:53 +0200

    added basic filling

---
+++ FillingService/EdgeBucket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightFiller.FillingService
+{
+    public class EdgeBucket
+    {
+        public int yMax { get; set; }
+        public int yMin { get; set; }
+        public int x { get; set; }
+        public bool positiveSign { get; set; }
+        public int dX { get; set; }
+        public int dY { get; set; }
+        public int sum { get; set; }
+
+        public EdgeBucket(Line line)
+        {
+            var smallerY = line.Points[0].Y > line.Points[1].Y ? line.Points[1] : line.Points[0];
+            var greaterY = line.Points[0].Y > line.Points[1].Y ? line.Points[0] : line.Points[1];
+            yMin = smallerY.Y;
+            x = smallerY.X;
+            yMax = greaterY.Y;
+            positiveSign = (greaterY.X - smallerY.X) > 0;
+            dX = Math.Abs(greaterY.X - smallerY.X);
+            dY = greaterY.Y - smallerY.Y;
+            sum = 0;
+        }
+    }
+}
--- FillingService/FillingService.cs
+++ FillingService/FillingService.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace LightFiller.FillingService
 {
     public class FillingService
     {
+        private List<EdgeBucket> EdgeTable = new List<EdgeBucket>();
+        private List<EdgeBucket> ActiveEdgeTable = new List<EdgeBucket>();
+
         public FillingService()
         {
 
+        }
+
+        public void InitTables(Polygon polygon)
+        {
+            EdgeTable.Clear();
+            ActiveEdgeTable.Clear();
+            foreach (var line in polygon.Edges)
+            {
+                EdgeTable.Add(new EdgeBucket(line));
+            }
+            EdgeTable.OrderBy(e => e.yMin);
         }
     }
 }
